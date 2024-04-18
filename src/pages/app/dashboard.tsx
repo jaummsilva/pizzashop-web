174c57859f85d542612beb6a9d50a381aa85d60e
João Vitor da Silva <150824087+sz-joao-silva@users.noreply.github.com>
2024-04-18 08:56:28 -0300

    first commit

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Dashboard() {
+  return (
+    <div>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async'
+
+export function SignIn() {
+  return (
+    <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <h1>Signin</h1>
+    </div>
+  )
+}
